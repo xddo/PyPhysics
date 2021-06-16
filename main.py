ce291a030171a74a9535ac62080a498a6cd596e1
xddo <xddo@protonmail.com>
2021-06-15 17:52:06 -0700

    fixed minor bugs

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 import pygame
+class Particle:
+    def __init__(self, coordinates, size):
+        self.x = coordinates[0]
+        self.y = coordinates[1]
+        self.size = size
+        self.color = (255, 0, 0)
+        self.thickness = 1
+
+def display(self):
+        pygame.draw.circle(screen, self.color, (self.x, self.y), self.size, self.thickness)
 
 (width, height) = (400, 400)
-BLACK = (255, 255, 255)
+WHITE = (255, 255, 255)
 
+pygame.init()
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Physics Simulation")
-screen.fill(BLACK)
+screen.fill(WHITE)
+
+first_particle = Particle((100, 50), 5)
+display(first_particle)
 
 pygame.display.flip()
 
