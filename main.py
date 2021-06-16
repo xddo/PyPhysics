1cc38b8189e463e7cde747fa3a288eeeb1eaac8c
xddo <xddo@protonmail.com>
2021-06-15 22:46:40 -0700

    added mouse support and mouse/particle interaction on-click

--- main.py
+++ main.py
@@ -89,6 +89,16 @@ def add_vectors(vector1, vector2):
     angle = 0.5 * math.pi - math.atan2(y, x)
     return (angle, length)
 
+# find_particle()
+# Parameters: particles array, (x, y) position
+# For each particle,
+# Check if distance from mouse to particle's center < particle size
+def find_particle(particles, x, y):
+    for p in particles:
+        if math.hypot(p.x - x, p.y - y) <= p.size:
+            return p
+    return None
+
 pygame.init()
 width = 640
 height = 480
@@ -108,7 +118,7 @@ drag = 0.999
 # Elasticity = loss of speed experienced when hitting boundary
 elasticity = 0.75
 
-number_particles = 10
+number_particles = 4
 particles = []
 
 for i in range(number_particles):
@@ -124,6 +134,7 @@ for i in range(number_particles):
 
 running = True
 clock = pygame.time.Clock()
+selected_particle = None
 while running: 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -131,12 +142,30 @@ while running:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            (x_pos, y_pos) = pygame.mouse.get_pos()
+            selected_particle = find_particle(particles, x_pos, y_pos)
+        elif event.type == pygame.MOUSEBUTTONUP:
+            selected_particle = None
+
+    # If clicking on particle,
+    # Get mouse's (x, y) position,
+    # Find distance from particle -> mouse position,
+    # Then, create vector (angle, length) that joins the two points
+    # This allows particle to be released w/ speed equal to mouse speed at time of release
+    if selected_particle:
+        (x_pos, y_pos) = pygame.mouse.get_pos()
+        dx = x_pos - selected_particle.x
+        dy = y_pos - selected_particle.y
+        selected_particle.angle = math.atan2(dy, dx) + 0.5 * math.pi
+        selected_particle.speed = math.hypot(dx, dy) * 0.1
 
     screen.fill(BLACK)
 
-    for p in particles:
-        p.move()
-        p.bounce()
+    for i, p in enumerate(particles):
+        if p != selected_particle:
+            p.move()
+            p.bounce()
         p.display()
 
     pygame.display.flip()
